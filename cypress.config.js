c7b29bf95fe5baa2a0e1dce6312a2138ea7bb0da
C98605A <@Dj11219>
2023-10-04 23:45:05 -0300

    Ajuste para execução na esteira

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,14 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import addContext from 'mochawesome/addContext';
 import './commands'
-import 'cypress-mochawesome-reporter/register';
+
+Cypress.on("test:after:run", (test, runnable) => {
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+    addContext({test}, videoUrl)
+})
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
